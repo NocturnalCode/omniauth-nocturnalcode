17aa0707898418d9fc315eecf189296ed82f354b
Daniel Brooker <dan@nocturnalcode.com>
2013-07-16 12:20:37 +1200

    Change raw_info address
    Change api/me to oauth/me

--- lib/omniauth/strategies/nocturnalcode.rb
+++ lib/omniauth/strategies/nocturnalcode.rb
@@ -21,7 +21,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/api/me').parsed
+        @raw_info ||= access_token.get('/oauth/me').parsed
       end
       
     end
